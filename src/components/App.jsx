d668bad1a6623f019dcf5f6022ab60f424ad3791
Ryan <ryan@ryannd.com>
2021-08-13 13:44:44 -0500

    Added initial API call to app for getting all songs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import SongsTable from './SongsTable/SongsTable';
 
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            songs: []
+        };
     }
+
+    componentDidMount() {
+        this.getSongs();
+    }
+
+    async getSongs() {
+        try{
+            let response = await axios.get("http://127.0.0.1:8000/songs/");
+            this.setState({
+                songs: response.data
+            })
+        } catch (ex) {
+            console.log("API call failed.");
+        }
+    }
+
+    // newSong
+
+    // deleteSong
+
+    // updateSong
 
     render() {
         return (
-            <SongsTable songs={this.state} />
+            <SongsTable songs={this.state.songs} />
         )
     }
 }
--- src/components/SongsTable/SongsTable.jsx
+++ src/components/SongsTable/SongsTable.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Song from '../Song/Song';
 
 function SongsTable(props) {
-    const songDetail = props.songDetail;
-    const songDetails = songDetail.map((song) => {
+    const songDetails = props.songs.map((song) => {
+        debugger;
         return <Song song={song} />
     })
     return (
         <table>
         <tr>
             <th>Title</th>
-            <th>Artis</th>
+            <th>Artist</th>
             <th>Album</th>
             <th>Genre</th>
             <th>Release Date</th>
             <th>Likes</th>
         </tr>
-            {songDetails}
+        {songDetails}
         </table>
     );
 }
